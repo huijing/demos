8f301cabbd83b1cce1ebbbcf9095b191088d2c32
Chen Hui Jing <huijing.chen@jewelpaymentech.com>
2018-02-05 15:44:46 +0800

    Update dependencies

--- critical-request-header/gulpfile.js
+++ critical-request-header/gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('sass-prod', function () {
 gulp.task('scripts', function() {
   return gulp.src(['js/*.js', 'js/custom.js'])
   .pipe(babel({
-    presets: ['es2015']
+    presets: ['env']
   }))
   .pipe(concat('scripts.js'))
   .pipe(gulp.dest('./'))
@@ -59,7 +59,7 @@ gulp.task('scripts', function() {
 gulp.task('scripts-prod', function() {
   return gulp.src(['js/*.js', 'js/custom.js'])
   .pipe(babel({
-    presets: ['es2015']
+    presets: ['env']
   }))
   .pipe(concat('scripts.js'))
   .pipe(uglify())
