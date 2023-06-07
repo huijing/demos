6c05d1954006cd602fedb904c9eb8423c6f4b8a2
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-06-07 21:06:41 +0800

    Update styles based on designer feedback

--- variable-fonts/js/custom.js
+++ variable-fonts/js/custom.js
@@ -3,18 +3,26 @@ const opszInput = document.getElementById("opsz");
 const spacInput = document.getElementById("space");
 const wghtInput = document.getElementById("weight");
 
-softInput.addEventListener("input", function (event) {
-  document.documentElement.style.setProperty("--soft", event.target.value);
-});
+if (softInput) {
+  softInput.addEventListener("input", function (event) {
+    document.documentElement.style.setProperty("--soft", event.target.value);
+  });
+}
 
-opszInput.addEventListener("input", function (event) {
-  document.documentElement.style.setProperty("--opsz", event.target.value);
-});
+if (opszInput) {
+  opszInput.addEventListener("input", function (event) {
+    document.documentElement.style.setProperty("--opsz", event.target.value);
+  });
+}
 
-spacInput.addEventListener("input", function (event) {
-  document.documentElement.style.setProperty("--spac", event.target.value);
-});
+if (spacInput) {
+  spacInput.addEventListener("input", function (event) {
+    document.documentElement.style.setProperty("--spac", event.target.value);
+  });
+}
 
-wghtInput.addEventListener("input", function (event) {
-  document.documentElement.style.setProperty("--wght", event.target.value);
-});
+if (wghtInput) {
+  wghtInput.addEventListener("input", function (event) {
+    document.documentElement.style.setProperty("--wght", event.target.value);
+  });
+}
